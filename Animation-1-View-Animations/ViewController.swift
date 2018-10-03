d497eda28ed5f0b331dd37d2ac43b028dfdd1c79
Ilqar Ilyasov <ilqar_ilyasov@yahoo.com>
2018-10-02 20:57:58 -0500

    Initial Commit

---
+++ Animation-1-View-Animations/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Animation-1-View-Animations
+//
+//  Created by Ilgar Ilyasov on 10/2/18.
+//  Copyright © 2018 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
